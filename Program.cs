1ef79f49b2b9ea3ee08962e0ed49d9578c92f171
bosko-d <boskodanilovic@outlook.com>
2020-03-12 17:02:41 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Threading;
+
+namespace MULTI_THREADED_TCP_SERVER_USING_DOTNET_CORE
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread t = new Thread(delegate ()
+            {
+                Server myServer = new Server("172.17.153.129", 13000);
+            });
+            t.Start();
+
+            Console.WriteLine("Server Started....");
+            Console.ReadLine();
+        }
+    }
+}
